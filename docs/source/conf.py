b9aa577f1d5f191f336319a949c9628685db2117
dan-slater <83245263+dan-slater@users.noreply.github.com>
2022-05-13 17:05:22 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-# sys.path.insert(0, os.path.abspath('../../src'))
+sys.path.insert(0, os.path.abspath('../..'))
 
 
 # -- Project information -----------------------------------------------------
@@ -31,8 +31,8 @@ release = '0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    # 'sphinx.ext.duration',
-    'sphinx.ext.doctest',
+    'readthedocs_ext.readthedocs',
+    'sphinx_toolbox.more_autodoc',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
 ]
